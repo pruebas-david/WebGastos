a05352150de1cb6ade1bdeffd59f6451190b376c
David Rubio Lastanao <drlastanao@hotmail.com>
2019-08-04 18:21:03 +0200

    campos requeridos

--- WebGastos/Models/Gasto.cs
+++ WebGastos/Models/Gasto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +10,13 @@ namespace WebGastos.Models
     public class Gasto
     {
         public int IdGasto { get; set; }
+        [Required]
         public string Importe { get; set; }
         public System.DateTime? Fecha { get; set; }
+        [Required]
         public string Descorta { get; set; }
+        [Required]
+
         public string Etiquetas { get; set; }
 
 

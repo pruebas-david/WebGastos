03a3e919e577850a694d03faa25baee5b664b743
David Rubio Lastanao <drlastanao@hotmail.com>
2019-08-04 13:55:28 +0200

    Jquery mobile

---
+++ WebGastos/Controllers/GastosController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebGastos.Controllers
+{
+    public class GastosController : Controller
+    {
+        // GET: Gastos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Gastos/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Gastos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Gastos/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Gastos/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Gastos/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Gastos/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Gastos/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
